8e3743477199118d7f501b4c08509ac6d3b49418
Henrique <henrique@wopus.com.br>
2025-06-25 21:29:59 -0300

    Limpando arquivos desnecesários do starter kit, configurando tailwind, cores, fontes e layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,16 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Quinho Pereira</title>
+
+        <x-assets-head/>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
         {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+    </body>
+</html>
